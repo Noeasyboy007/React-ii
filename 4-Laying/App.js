5c11244592996de2b7508b7516d2d8be03e66cf1
Aritra Bera <aritrabera69@gmail.com>
2024-08-14 05:50:28 +0530

    1st commit

--- 4-Laying/App.js
+++ 4-Laying/App.js
@@ -1,31 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 const heading1 = React.createElement(
-    "h1",
-    {
-      id: "title",
-      key:"h2",
-    },
-    "heading1"
-  );
-  const heading2 = React.createElement(
-    "h2",
-    {
-      id: "title",
-      key :"h2",
-    },
-    "heading2"
-  );
-  
-  const container = React.createElement(
-    "div",
-    {
-      id: "container",
-    },
-    [heading1, heading2]
-  );
-  
-  // create root using createRoot
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  // passing react element inside root
-  root.render(container);
+  "h1",
+  {
+    id: "title1",
+    key: "h1",
+  },
+  "heading1"
+);
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "title2",
+    key: "h2",
+  },
+  "heading2"
+);
+
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading1, heading2]
+);
+
+// JSX ->??
+const heading3 = <h3 id="titel3" key="h3">heading3</h3>
+
+// create root using createRoot
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// passing react element inside root
+root.render(container);
