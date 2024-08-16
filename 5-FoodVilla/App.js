79543613b33d1a3225efb9ca773e1c6fb71339f1
Aritra Bera <aritrabera69@gmail.com>
2024-08-17 03:42:59 +0530

    create Application Layout

--- 5-FoodVilla/App.js
+++ 5-FoodVilla/App.js
@@ -20,17 +20,68 @@ const Header = () => {
           <li>Home</li>
           <li>About</li>
           <li>Contact</li>
+          <li>Cart</li>
         </ul>
       </div>
     </div>
   )
 };
 
+//Restraunt card data
+const burgerKing = {
+  name: "Burger King",
+  image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/972a37599772cdc7df93a0855ad87591",
+  cusines: ["Burger", "American"],
+  rating: "4.2"
+}
+
+
+//Restraunt Card 
+const RestrauntCard = () => {
+  return (
+    <div className="card">
+      <img src={burgerKing.image} />
+      <h2>{burgerKing.name}</h2>
+      <h3>{burgerKing.cusines.join(", ")}</h3>
+      <h4>{burgerKing.rating} stars</h4>
+    </div>
+  )
+}
 
 // Body component
 const Body = () => {
   return (
-    <h4>Body</h4>
+    <div className="restraunt-list">
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+      <RestrauntCard />
+    </div>
   )
 };
 
@@ -55,4 +106,4 @@ const AppLayout = () => {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeaderComponent />);
+root.render(<AppLayout />);
