575cd72b4f23d8b0df052e607acaff8acd4d2130
Aritra Bera <aritrabera69@gmail.com>
2024-08-20 03:08:25 +0530

    create fil structure to move and wrap our file's

---
+++ 5-FoodVilla/src/components/RestrauntCard.js
@@ -0,0 +1,20 @@
+import { restrauntList, IMG_URL } from "./config"
+
+restrauntList
+
+//Restraunt Card 
+const RestrauntCard = ({ cloudinaryImageId, name, cuisines, areaName, costForTwo }) => {
+    // const {}=restraunt.info
+    return (
+        <div className="card">
+            <img src={IMG_URL + cloudinaryImageId} />
+            <h2>{name}</h2>
+            <h3>{cuisines.join(", ")}</h3>
+            <h4>{areaName}</h4>
+            <h4>{costForTwo}</h4>
+        </div>
+    )
+}
+
+export default RestrauntCard;
+
