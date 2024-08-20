94aa4e7ce41be985be548b89f735bd6b44789675
Aritra Bera <aritrabera69@gmail.com>
2024-08-21 04:15:01 +0530

    implement search box

--- 5-FoodVilla/src/components/Body.js
+++ 5-FoodVilla/src/components/Body.js
@@ -1,17 +1,34 @@
 import { restrauntList } from "./config";
-import RestrauntCard  from "./RestrauntCard";
+import RestrauntCard from "./RestrauntCard";
+import { useState } from "react"
+
 
 // Body component
 const Body = () => {
+    const [searchInput, setSearchInput] = useState()
     return (
-        <div className="restraunt-list">
-            {
-                restrauntList.map((restraunt) => {
-                    return <RestrauntCard {...restraunt.info} key={restraunt.info.id} />
-                })
-            }
+        <>
+            <div className="search-container">
+                <input
+                    type="text"
+                    className="search-input"
+                    placeholder="Search"
+                    value={searchInput}
+                    onChange={(e) => {
+                        setSearchInput(e.target.value)
+                    }}
+                />
+                <button className="search-btn">Search</button>
+            </div>
+            <div className="restraunt-list">
+                {
+                    restrauntList.map((restraunt) => {
+                        return <RestrauntCard {...restraunt.info} key={restraunt.info.id} />
+                    })
+                }
 
-        </div>
+            </div>
+        </>
     )
 };
 
