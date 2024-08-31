7c345d82336cf2b4ed40f6894166aa77e8b79a8b
Aritra Bera <aritrabera69@gmail.com>
2024-09-01 00:04:40 +0530

    classComponents inside using useState setCount

--- 5-FoodVilla/src/components/ProfileClass.js
+++ 5-FoodVilla/src/components/ProfileClass.js
@@ -5,15 +5,22 @@ class ProfileClassComponents extends React.Component {
         super(props);
         //create State
         this.state = {
-            count: 1,
+            count: 0,
+
         };
     };
     render() {
+        const { count } = this.state;
         return (
             <div>
                 <h1>Profile classs Component</h1>
                 <h2>Name: {this.props.name}</h2>
-                <h2>Count:{this.state.count}</h2>
+                <h2>Count:{count}</h2>
+                <button onClick={() => {
+                    //We do not muted state directly
+                    //never do this.state=something
+                    this.setState({ count: count + 1 })
+                }}>Click</button>
             </div>
         )
     }
