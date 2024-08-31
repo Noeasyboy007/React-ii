9f7c3c7b9d49bae78019d23f4fba9ace8c156224
Aritra Bera <aritrabera69@gmail.com>
2024-08-31 23:45:28 +0530

    classComponents inside using useState

--- 5-FoodVilla/src/components/Profile.js
+++ 5-FoodVilla/src/components/Profile.js
@@ -1,8 +1,12 @@
+import { useState } from 'react';
+
 const ProfileFunctionalComponents = (props) => {
+    const [count]=useState(0);
     return (
         <div>
             <h2>Profile Functional Component</h2>
             <h3>Name:{props.name}</h3>
+            <h3>Count:{count}</h3>
         </div>
     )
 }
--- 5-FoodVilla/src/components/ProfileClass.js
+++ 5-FoodVilla/src/components/ProfileClass.js
@@ -1,11 +1,19 @@
 import React from "react"
 
 class ProfileClassComponents extends React.Component {
+    constructor(props) {
+        super(props);
+        //create State
+        this.state = {
+            count: 1,
+        };
+    };
     render() {
         return (
             <div>
                 <h1>Profile classs Component</h1>
                 <h2>Name: {this.props.name}</h2>
+                <h2>Count:{this.state.count}</h2>
             </div>
         )
     }
